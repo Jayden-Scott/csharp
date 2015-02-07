b5d611d6aef46bef23b07f50303c20cf26b1daac
Jayden Scott <jayden_scott@hotmail.com>
2015-02-07 20:42:38 +0000

    Initial Commit

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "jayden";
+
+            Console.Title = "Startup.exe";
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            Console.WriteLine("Starting setup...");
+            Console.WriteLine("Retrieving framework information...");
+            Console.WriteLine("Hello " + name);
+            Console.Read();
+
+            string enter = Console.ReadLine();
+
+            if (enter = "Help" || enter = "HELP")
+            {
+                Console.WriteLine("tutjyj");
+            }
+        }
+    }
+}
